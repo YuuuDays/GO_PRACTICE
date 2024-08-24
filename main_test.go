acd04395a8b37a32b8e5489644e600e78c830cc4
Yuu Hikida <ponponda0103@gmail.com>
2024-08-24 19:29:28 +0900

    create TestCode

--- main_test.go
+++ main_test.go
@@ -2,9 +2,17 @@ package main
 
 import "testing"
 
+/*
+テストを書く際のルール
+xxx_test.goのようなはファイル名
+関数名は必ず"Test"から始まる
+テスト関数の引数は "t *testing.T"
+import "testing"の記載が必要
+*/
+
 func TestMain(t *testing.T) {
 	got := Hello()
-	want := "Hello,World"
+	want := "Hello,World!"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
