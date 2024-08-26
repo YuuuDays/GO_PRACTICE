d604f51b1b51b1e6af34dd7bacb4cb6029d4ff79
hikida <ponponda0103@gmail.com>
2024-08-26 20:01:12 +0900

    change of replace

--- Arrays_and_Slices/sum_test.go
+++ Arrays_and_Slices/sum_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3, 4, 5}
 	// numbers := [...]int{1, 2, 3, 4, 5}
 
 	got := Sum(numbers)
@@ -14,10 +14,21 @@ func TestSum(t *testing.T) {
 	if got != want {
 		t.Errorf("got :(%q), want:(%q)", got, want)
 	}
+
+	t.Run("aaaa", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got:(%q),want:(%q)", got, want)
+		}
+	})
 }
 
 func BenchmarkRepeat(b *testing.B) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3, 4, 5}
 	for i := 0; i < b.N; i++ {
 		Sum(numbers)
 	}
