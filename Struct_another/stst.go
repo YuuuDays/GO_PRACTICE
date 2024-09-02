e043a99c9e627c5e5bbe535f1cc1462224ea6347
hikida <ponponda0103@gmail.com>
2024-09-02 21:32:04 +0900

    practice struct2

--- Struct_another/stst.go
+++ Struct_another/stst.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 // ↓これは構造体を使った変数の宣言,インスタンス
-
+// 無名構造体
 // var coordinate struct {
 // 	lat float64
 // 	lng float64
@@ -11,7 +11,7 @@ import "fmt"
 
 //構造体自体を定義してるらしい
 // 型名は大文字で始めるとよい、大文字で始めるとエクスポートされる
-type coordinate struct {
+type Coordinate struct {
 	lat float64
 	lng float64
 }
@@ -20,9 +20,20 @@ func main() {
 	// coordinate.lat = 3.141491919
 	// coordinate.lng = 6.6464646464
 	// ↓これが宣言としては一般的
-	var s coordinate
+	var s Coordinate
 	s.lat = 3.14
 	s.lng = 6.666
 	fmt.Println(s.lat, s.lng)
 	fmt.Println(s)
+	fmt.Println("-------------------------------")
+
+	sibuya := Coordinate{lat: 1.111111, lng: 2.22222222}
+	fmt.Println(sibuya.lat)
+	fmt.Println(sibuya)
+
+	fmt.Println("-------------------------------")
+
+	tanaka := Coordinate{4.444444, 5.5555555}
+	fmt.Println(tanaka.lat)
+	fmt.Println(tanaka)
 }
