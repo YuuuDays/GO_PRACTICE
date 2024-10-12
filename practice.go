6e1189c27e9953f7518c5b153d689f0667600ac1
hikida <ponponda0103@gmail.com>
2024-10-12 21:20:16 +0900

    refactring

--- practice.go
+++ practice.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
-func main() {
+// 構造体(スライスに関して)
+func slice_confirmation() {
 	type User struct {
 		Name string
 		Age  int
@@ -25,4 +29,41 @@ func main() {
 	fmt.Println("変更前(ポインタ)", u_2[0].Name, ":", u_2[0].Age) //変更前(ポインタ) torou : 10
 	u_2[0].Name = "EIDA"
 	fmt.Println("変更後(ポインタ)", u_2[0].Name, ":", u_2[0].Age) //変更後(ポインタ) TANAKA : 10
+
+}
+
+/*-------------------------------------------------------------------------------*/
+/*Go言語ではインターフェースや構造体、そしてメソッドの定義はトップレベルでしかできない*/
+/*-------------------------------------------------------------------------------*/
+type Storage interface {
+	Save(data string)
+}
+
+// FileStorage 構造体
+type FileStorage struct{}
+
+// FileStorageのSaveメソッド（インターフェースの実装）
+func (fs FileStorage) Save(data string) {
+	fmt.Println("Saving to file:", data)
+}
+
+/*↑ 関数の中で関数を定義してはならないので
+関数内で構造体メソッド定義を行うとエラー*/
+
+func type_confirmation() {
+	var number int = 42
+	fmt.Println("number->", reflect.TypeOf(number))
+
+	//低結合性と柔軟性
+	var storage Storage = FileStorage{}
+	storage.Save("OOENTKYK!")
+
+	fmt.Println("storage->", reflect.TypeOf(storage)) // main.FileStorage
+
+}
+
+func main() {
+	//slice_confirmation()
+	type_confirmation()
+
 }
