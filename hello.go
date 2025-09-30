8a2887784208c0a263a04a57002a3c3e12d7c354
hikida <ponponda0103@gmail.com>
2025-09-30 21:12:39 +0900

    テストコードも書こう!

--- hello.go
+++ hello.go
@@ -5,7 +5,11 @@ import (
 	"time"
 )
 
+func Hello() string {
+	return "Hello,World!"
+}
+
 func main() {
-	fmt.Println("Hello,World!")
+	fmt.Println(Hello())
 	fmt.Println("time is money", time.Now())
 }
