21ee7945ae801ffed98b4f014dc7b9d3af622dc7
hikida <ponponda0103@gmail.com>
2025-10-08 21:03:01 +0900

    green

--- src/ArraysAndslices/sum.go
+++ src/ArraysAndslices/sum.go
@@ -1,8 +1,8 @@
 package arrays
 
-func Sum(arrays [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
-	for _, num := range arrays {
+	for _, num := range numbers {
 		sum += num
 	}
 	return sum
