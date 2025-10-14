a77452c3105a37a6ed24283a027ca002565e0f75
hikida <ponponda0103@gmail.com>
2025-10-14 23:01:40 +0900

    構造体学習

--- src/structs/struct_test.go
+++ src/structs/struct_test.go
@@ -2,6 +2,12 @@ package structs
 
 import "testing"
 
+// 構造体
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
 func TestPermiment(t *testing.T) {
 	got := Perimeter(10.0, 10.0)
 	want := 40.0
