8e6d534a2256a801bfd9a54328d4c1763cd52a50
hikida <ponponda0103@gmail.com>
2025-10-19 16:11:33 +0900

    red2

--- src/pointer/wallet.go
+++ src/pointer/wallet.go
@@ -1,13 +1,13 @@
 package pointer
 
 type Wallet struct {
-	money int
+	balance int
 }
 
-func (w Wallet) Deposit(x int) {
-	w.money = x
+func (w Wallet) Deposit(amount int) {
+	w.balance += amount
 }
 
 func (w Wallet) Balance() int {
-	return w.money
+	return w.balance
 }
