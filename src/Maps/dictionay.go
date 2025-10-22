805bb2013d46a72d89a6345941edfb31f7bf670e
hikida <ponponda0103@gmail.com>
2025-10-22 22:52:19 +0900

    飛んでgreen

--- src/Maps/dictionay.go
+++ src/Maps/dictionay.go
@@ -8,11 +8,17 @@ func Search(dictionary map[string]string, word string) string {
 
 type Dictionary map[string]string
 
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 func (d Dictionary) Search(word string) (string, error) {
 	// 第一引数に値、第二引数に数値が格納されたかどうかのboolean値がはいるらしい
 	definition, ok := d[word]
 	if !ok {
-		return "", errors.New("could not find the word you were looking for")
+		return "", ErrNotFound
 	}
 	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) {
+	d[word] = definition
 }
