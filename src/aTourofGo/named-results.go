7188337ed063015f70edd15781f190172cfc58a9
hikida <ponponda0103@gmail.com>
2025-10-27 23:35:07 +0900

    サーバ起動&クライアント離脱をctx.Done()でキャッチできることを確認

--- src/aTourofGo/named-results.go
+++ src/aTourofGo/named-results.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
+/*2000年後の僕へ
+go run xxx.go
+上記だと単体のgoファイルしかビルドされないぞ
+go run .
+とか
+go run *.go
+でまとめてコンパイルしよう
+*/
 // naked return とかいうものですぅ
 func CallTwoArgs(sum int) (x, y int) {
 	x = sum * 2
@@ -9,6 +21,23 @@ func CallTwoArgs(sum int) (x, y int) {
 	return
 }
 
+var (
+	ToBe bool = false
+	moji rune = 'い'
+)
+
 func main() {
-	fmt.Println(CallTwoArgs(10))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context() // リクエストの生存監視
+
+		select {
+		case <-time.After(3 * time.Second):
+			fmt.Println(w, "処理完了")
+		case <-ctx.Done(): //クライアントとが途中離脱するとここにはいる
+			fmt.Println("中断理由:", ctx.Err())
+			return
+		}
+	})
+	fmt.Println("server start:8080")
+	http.ListenAndServe(":8080", nil)
 }
