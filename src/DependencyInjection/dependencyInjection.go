596a27cc04278ec690bf2e6eccd66ee48fc1b51c
hikida <ponponda0103@gmail.com>
2025-11-04 12:54:32 +0900

    green

--- src/DependencyInjection/dependencyInjection.go
+++ src/DependencyInjection/dependencyInjection.go
@@ -1,10 +1,10 @@
 package dependencyinjection
 
 import (
-	"bytes"
-	"fmt"
+    "fmt"
+    "io"
 )
 
-func Greet(writer *bytes.Buffer, name string) {
-	fmt.Fprintf(writer, "Hello, %s", name)
+func Greet(writer io.Writer, name string) {
+    fmt.Fprintf(writer, "Hello, %s", name)
 }
--- src/hello.go
+++ src/hello.go
@@ -2,32 +2,14 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/YuuHikida/GO_PRACTICE/src/pra_interface"
+	"io"
+	"os"
 )
 
-const englishHelloPrefix = "Hello,"
-
-func Hello(name string) string {
-	if name == "" {
-		name = " World"
-
-	}
-	return englishHelloPrefix + name
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
 func main() {
-	blog := pra_interface.Blog{
-		Title:   "taitorudesu!!",
-		Content: "naiyoudessssssssssssss",
-	}
-
-	blog2 := pra_interface.Blog2{
-		Title:     "タイトルです。",
-		Paragraph: []string{"いち", "にい", "さん・・・？"},
-	}
-
-	fmt.Println(blog.GetFullArticle())
-	fmt.Println("--------------")
-	fmt.Println(blog2.GetFullArticle())
+	Greet(os.Stdout, "Elodie")
 }
