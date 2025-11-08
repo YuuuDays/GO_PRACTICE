232c13ffeb3757d5c72447218a18c032b77bf8a4
hikida <ponponda0103@gmail.com>
2025-11-08 22:20:04 +0900

    all green

--- src/hello.go
+++ src/hello.go
@@ -25,6 +25,39 @@ func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
 
+type CountdownOperationsSpy struct {
+	Calls []string
+}
+
+func (s *CountdownOperationsSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
 // func Greet(writer io.Writer, name string) {
 // 	fmt.Fprintf(writer, "Hello, %s", name)
 // }
@@ -51,12 +84,12 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
+
 	sleeper.Sleep()
 	fmt.Fprint(out, finalWord)
-
 }
 
 func main() {
-	sleeper := &SpySleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
--- src/hello_test.go
+++ src/hello_test.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
+	"time"
 )
 
 /*
@@ -10,24 +12,59 @@ buffer には
 Countdown 関数が書き込んだ「バイト列（文字列）」がメモリ上に溜まっている
 */
 func TestCountdown(t *testing.T) {
-	// goの標準パッケージbytesにある「バイト配列を内蔵した可変の入れ物」
-	//&bytes.Buffer{} としているのは「そのバッファのポインタ」を作ってる
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
 
-	Countdown(buffer, spySleeper)
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		// goの標準パッケージbytesにある「バイト配列を内蔵した可変の入れ物」
+		//&bytes.Buffer{} としているのは「そのバッファのポインタ」を作ってる
+		buffer := &bytes.Buffer{}
+		spySleeper := &SpySleeper{}
 
-	// buffer.String() を呼ぶと、その溜まったバイト列が Go の string として取り出せる
-	got := buffer.String()
-	want := `3
+		Countdown(buffer, spySleeper)
+
+		// buffer.String() を呼ぶと、その溜まったバイト列が Go の string として取り出せる
+		got := buffer.String()
+		want := `3
 2
 1
 Go!`
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 
-	if spySleeper.Calls != 4 {
-		t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
+		if spySleeper.Calls != 4 {
+			t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
+		}
+	})
+
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &CountdownOperationsSpy{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
 	}
 }
