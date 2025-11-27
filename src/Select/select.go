fdc070dcb38abf5eb8464c231f25b8ef4bf3b4ce
hikida <ponponda0103@gmail.com>
2025-11-27 21:04:33 +0900

    greeeen

--- src/Select/select.go
+++ src/Select/select.go
@@ -1,5 +1,24 @@
 package selectPRA
 
+import (
+	"net/http"
+	"time"
+)
+
 func Racer(a, b string) (winner string) {
-	return
+	// URLを取得する直前の時間を記録する
+	startA := time.Now()
+	// URLをコンテンツを取得する
+	http.Get(a)
+	aDuration := time.Since(startA)
+
+	startB := time.Now()
+	http.Get(b)
+	bDuration := time.Since(startB)
+
+	if aDuration < bDuration {
+		return a
+	}
+
+	return b
 }
