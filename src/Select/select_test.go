191306b7782b55fadb632bf109e6ccfdb83d20e7
hikida <ponponda0103@gmail.com>
2025-11-28 23:58:48 +0900

    red

--- src/Select/select_test.go
+++ src/Select/select_test.go
@@ -1,8 +1,22 @@
 package selectPRA
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
+
+	/*
+		httptest.NewServerはテスト用サーバ
+	*/
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
 	slowURL := "http://www.facebook.com"
 	fastURL := "http://www.quii.co.uk"
 
