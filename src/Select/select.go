726e3009202aee0c42e2ba15d0d27cb20b5bca1e
hikida <ponponda0103@gmail.com>
2025-12-02 22:04:28 +0900

    green

--- src/Select/select.go
+++ src/Select/select.go
@@ -6,19 +6,33 @@ import (
 )
 
 func Racer(a, b string) (winner string) {
-	// URLを取得する直前の時間を記録する
-	startA := time.Now()
-	// URLをコンテンツを取得する
-	http.Get(a)
-	aDuration := time.Since(startA)
-
-	startB := time.Now()
-	http.Get(b)
-	bDuration := time.Since(startB)
-
+	aDuration := measureResponseTime(a)
+	bDuration := measureResponseTime(b)
 	if aDuration < bDuration {
 		return a
 	}
-
 	return b
 }
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
+
+}
+
+// 	// URLを取得する直前の時間を記録する
+// 	startA := time.Now()
+// 	// URLをコンテンツを取得する
+// 	http.Get(a)
+// 	aDuration := time.Since(startA)
+
+// 	startB := time.Now()
+// 	http.Get(b)
+// 	bDuration := time.Since(startB)
+
+// 	if aDuration < bDuration {
+// 		return a
+// 	}
+
+// 	return b
+// }
--- src/Select/select_test.go
+++ src/Select/select_test.go
@@ -17,6 +17,10 @@ func TestRacer(t *testing.T) {
 		w.WriteHeader(http.StatusOK)
 	}))
 
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
 	slowURL := "http://www.facebook.com"
 	fastURL := "http://www.quii.co.uk"
 
@@ -26,4 +30,7 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
+
+	slowServer.Close()
+	fastServer.Close()
 }
